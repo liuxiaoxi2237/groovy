01eed8a6b557718df1da36082c0771c95d74baca
liuxiaoxi2237 <david.g.liu@email.com>
2018-05-29 14:44:11 +0800

    change directory var to vars

---
+++ vars/newpipeline.groovy
@@ -0,0 +1,54 @@
+import static com.sds.fortiScan.*;
+def call(body) {
+	
+		def config = [:]
+		body.resolveStrategy = Closure.DELEGATE_FIRST
+		body.delegate = config
+		body()
+	
+		pipeline {
+			agent = any
+			tools {
+				maven 'MAVEN3.0'
+				jdk 'JDK1.8'
+				}
+		
+			   stages {
+					stage('initialize') {
+						steps {
+							sh '''
+							PATH = $PATH:/opt/HPE_Security/Fortify_SCA_and_Apps_17.10/bin
+							export PATH
+							echo $PATH
+							pwd
+							'''
+							git credentialsId: 'd8410c40-131c-457b-b80f-096a23f20c0a', url: 'http://10.62.81.24/binbinw/data-transfer-service.git'
+							
+							sh '''
+							mvn clean
+							'''                
+						}
+					}
+		
+		stage('build') {
+			steps {
+				sh '''
+                    mvn install
+					mkdir app-lib
+					find /var/lib/jenkins/workspace/${params.projectName}/${params.appName}  -name *.jar -exec cp {} ./app-lib  \;
+					mkdir maven-lib
+					find ${params.maven_repo}  -name *.jar -exec cp {} ./maven-lib \;
+                    '''
+			}
+		}
+		
+		stage('scan') {
+				steps {
+					fscan(script,${params.appName},${params.projectName})
+				}
+		}
+					
+		}
+	}
+}
+		
